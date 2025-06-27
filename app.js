46cdf3b4c7105f1b5fd5744306321f8b61a28866
Pablo <pablogn06.pn@gmail.com>
2025-06-27 09:29:11 -0300

    Fim da aula 3

--- app.js
+++ app.js
@@ -7,13 +7,47 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
+function limparCampo() {
+    let chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Descubra o número secreto entre 1 e 10');
+}
+
+
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Parabéns!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas =`Você acertou em ${tentativas} ${palavraTentativa}.`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor que ' + chute);
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior que ' + chute);
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    tentativas = 1;
+    limparCampo();
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
+    
 }
 
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+exibirMensagemInicial();
 
-exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-exibirTextoNaTela('p', 'Descubra o número secreto entre 1 e 10');
 
---
+++ js-curso-2-desafio_3/app.js
@@ -0,0 +1,42 @@
+// // Exercicio 1
+// function imc(altura, peso) {
+//     return peso / (altura * altura);
+// }
+
+// // Exercicio 2
+// function fatorial(numero) {
+//     if (numero === 0 || numero === 1) {
+//         return 1;
+//     }
+//     return numero * fatorial(numero - 1);
+// }
+// let entrada = prompt('Digite um número para calcular o fatorial:');
+// let numero = parseInt(entrada);
+// alert(`O fatorial de ${numero} é ${fatorial(numero)}`);
+
+// // Exercicio 3
+// function dolarReal(valor,cotacao) {
+//     return valor * cotacao;
+// }
+
+// // Exercício 4
+// function areaPerimetroRetangulo(base, altura) {
+//     let area = base * altura;
+//     let perimetro = 2 * (base + altura);
+//     alert(`Área: ${area}, Perímetro: ${perimetro}`);
+// }
+
+// // Exercício 5
+// function areaPerimetroCirculo(raio) {
+//     const pi = Math.PI;
+//     area = pi * raio * raio;
+//     perimetro = 2 * pi * raio;
+//     alert(`Área: ${area}, Perímetro: ${perimetro}`);
+// }
+
+// // Exercício 6
+// function tabuada(numero) {
+//     for (let i = 1; i <= 10; i++) {
+//         alert(`${numero} x ${i} = ${numero * i}\n`);
+//     }
+// }
