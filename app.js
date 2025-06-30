3c34a8730e9b0e9de2f5bb3afa589a32efd44fa5
Pablo Goncalves <pablo.goncalves@cepel.br>
2025-06-30 08:59:52 -0300

    Fim do curso

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ function gerarNumeroAleatorio() {
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto,'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function limparCampo() {
